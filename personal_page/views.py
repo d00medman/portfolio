f55de300ddac08401db7a8906d0fd53ad48bf6be
AlexMollohan <ajmollohan@gmail.com>
2017-07-10 00:32:33 -0400

    Fixed routing error and created path to include an html snippet

--- personal_page/views.py
+++ personal_page/views.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-def index(response):
-    return render(request, 'personal_page/home.html')
+def index(request):
+    return render(request, 'personal/home.html') # for future reference, if you start w/personal_page, maintain that consistancy
 # Create your views here.
