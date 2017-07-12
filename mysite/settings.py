0828a4016c62605f73dd6117e0c7a1b354a5640e
AlexMollohan <ajmollohan@gmail.com>
2017-07-12 17:37:03 -0400

    added config setups for blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # These dependencies must be manually installed, and
     'personal',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
