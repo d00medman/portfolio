10fa6c14bdc54b8536b000a5b1260819e23744df
AlexMollohan <ajmollohan@gmail.com>
2017-07-13 15:36:09 -0400

    SO question answered how to return linking datasets to my skills view
    working on inital configuration of portfolio app

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length = 32)
+    short_description = models.CharField(max_length = 200)
+    long_description = models.TextField()
+    
