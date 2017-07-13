1a721996f644d6b9631fa6e7aa966b92cc4f629f
AlexMollohan <ajmollohan@gmail.com>
2017-07-13 12:51:34 -0400

    creadted blog page, both for individual posts and the broader blog
    Next step would be building out a works page. Want to make this resemble my portfolio as much as humanly possible before deployment.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,11 @@ from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 urlpatterns = [
-    url(r'^$', ListView, ListView.as_view(queryset=Post.objects.all().order_by('-date')[:25],
-    template_name='blog/blog.html'))
+    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by('-date')[:25],
+    template_name='blog/blog.html')),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post,
+                                             template_name='blog/post.html'))
+    # ?P corresponds to named groups
+    # <pk> is the id column
+    # \d+ is a requirement for one or more digits
 ]
