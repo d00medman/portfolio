8523a28d671af169a3c13a675b00c0501467d258
AlexMollohan <ajmollohan@gmail.com>
2017-07-13 15:06:42 -0400

    Able to display technology and associated language, but little else
    should be sufficient for MVP, but want something a little nicer.

--- personal/urls.py
+++ personal/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from . import views
 from django.views.generic import ListView
-from personal.models import Language
+from personal.models import Language, Technology
 
 urlpatterns = [
     url(r'^$', views.index, name='index'), # Defining url patterns. As things stand, we have a start and an end
     url(r'^contact/', views.contact, name='contact'),
-    url(r'^skills/$', ListView.as_view(queryset=Language.objects.all(), template_name='personal/skills.html'))
+    url(r'^skills/$', ListView.as_view(queryset=Technology.objects.all(), template_name='personal/skills.html'))
 ]
