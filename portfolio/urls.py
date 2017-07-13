7e4b29bcd0c9c109f217fac231101272ee4e60fa
AlexMollohan <ajmollohan@gmail.com>
2017-07-13 16:07:28 -0400

    Able to disply individual projects

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,7 @@ from portfolio.models import Project
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Project.objects.all().order_by('-id')[:25],
-                                template_name='portfolio/portfolio.html'))
+                                template_name='portfolio/portfolio.html')),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Project,
+                                             template_name='portfolio/project.html'))                            
 ]
